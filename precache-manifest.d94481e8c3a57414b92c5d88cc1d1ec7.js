e9e974242ee64dc310caeeaaf7df1c9ff5adc7e1
Rudi Aj <rudi.aj@signet.hr>
2019-09-08 19:53:44 +0200

    Updates

---
+++ precache-manifest.d94481e8c3a57414b92c5d88cc1d1ec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91ff130677cd1e4882a896081df981a",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "88ac626b8ef8a089346a",
+    "url": "/Pokedex/static/css/main.70a48797.chunk.css"
+  },
+  {
+    "revision": "0208270ee6d44b3a836a",
+    "url": "/Pokedex/static/js/2.42eae0c6.chunk.js"
+  },
+  {
+    "revision": "88ac626b8ef8a089346a",
+    "url": "/Pokedex/static/js/main.cbe86abc.chunk.js"
+  },
+  {
+    "revision": "f9041e3ac75bdcea2b90",
+    "url": "/Pokedex/static/js/runtime~main.bc0aea47.js"
+  }
+]);
